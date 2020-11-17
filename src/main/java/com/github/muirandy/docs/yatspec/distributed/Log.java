1802da42848f8a506ce1822e58febb2ee0084881
Andy Muir <muirandy@yahoo.co.uk>
2020-11-17 19:55:09 +0000

    Allow instances of Log to compare for equality sensibly

--- src/main/java/com/github/muirandy/docs/yatspec/distributed/Log.java
+++ src/main/java/com/github/muirandy/docs/yatspec/distributed/Log.java
@@ -1,5 +1,8 @@
 package com.github.muirandy.docs.yatspec.distributed;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Log {
     private final String message;
     private final Object body;
@@ -16,5 +19,15 @@ public class Log {
 
     public Object getBody() {
         return body;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
     }
 }
